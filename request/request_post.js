04a1c69e10a69637b7d1ad95930d15a663559ba0
Abunuo <liangmingqing315@163.com>
2017-07-24 18:33:59 +0800

    reqest 更改

--- request/request_post.js
+++ request/request_post.js
@@ -1,25 +1,11 @@
 var fs = require('fs')
 var request = require('request')
 
-var url = "http://test.asos.upay360.cn/accounts/csv/upload"
+var url = "http://www.hao123.com"
 
 
-// fs.createReadStream('./request/load/30_re.csv').pipe(request.post(url, function(err, res, body){
-//   console.log(err);
-//   console.log(res.statusCode);
-//   console.log(JSON.parse(body));
-// }))
-
-fs.createReadStream('./request/load/30_re.csv').pipe(request.post(url))
-  .on('error', function(error) {
-    console.log(1);
-    console.log(error);
-  })
-  .on('response', function(response) {
-    console.log(2);
-    console.log(response.statusCode);
-  })
-  .on('body', function(body) {
-    console.log(3);
-    console.log(JSON.parse(body));
-  })
+fs.createReadStream('./request/load/30_re.csv').pipe(request.post(url, function(err, res, body){
+  console.log(err);
+  console.log(res.statusCode);
+  console.log(JSON.parse(body));
+}))
