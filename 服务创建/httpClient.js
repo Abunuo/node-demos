cdc8e56836796fe66ae9cdecfde9456324adc366
Abunuo <liangmingqing315@163.com>
2017-07-25 11:09:33 +0800

    add server/client

---
+++ 服务创建/httpClient.js
@@ -0,0 +1,26 @@
+var request = require('request')
+var http  = require('http')
+var req = http.request({host:'127.0.0.1',port:'8000',method:'POST'}, function(res) {
+  var body = '';
+  res.setEncoding( 'utf-8' );
+  res.on( 'data', function (chunk) {
+    body += chunk;
+  });
+  res.on( 'end', function () {
+    console.log( 'body');
+  });
+})
+req.write('123')
+req.end()
+
+
+// var data = ''
+// var req = request('http://127.0.0.1:8000')
+//     .on('data', function(chunk) {
+//         data+=chunk
+//     })
+//     .on('end', function(){
+//        console.log(data);
+//        console.log('断开连接');
+//     })
+//     .end("客户端断开连接")
--- 服务创建/httpServer.js
+++ 服务创建/httpServer.js
@@ -1,19 +1,29 @@
 var http = require('http');
 var _request = require('request')
 
+var chunk = ''
 http.createServer(function (request, response) {
+  request.on('data', function(chunk) {
+    response.write('服务端收到信息')
+    console.log(chunk.toString());
+  })
+  request.on('end', function(chunk) {
+    response.end('客户端断开连接')
+    console.log('请求结束');
+  })
+
   //发送 http 头部、 HTTP 状态值：200：OK、 内容类型：text/plain
   response.writeHead(200, {'Content-Type': 'text/plain'});
 
   //发送相应数据 “Hello World”
-  response.end('Hello World \n');
+  response.write('Hello World \n');
   // _request.get('http://asos.upay360.cn/accounts/csv/report?importId=30&isRecoverable=true',function(err, res, body) {
   //   response.end(body)
   // })
 
 }).listen(8000, '127.0.0.1', function() {
   //终端打印信息
-  console.log('Srever running at http:127.0.0.1:8000/');
+  console.log('Srever running at http://127.0.0.1:8000/');
 });
 
 //终端打印信息
