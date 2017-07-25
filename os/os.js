fab5d0cab9bbe2816f7bbaceec692ea7dcaaf0c4
Abunuo <liangmingqing315@163.com>
2017-07-25 10:10:55 +0800

    add os/

---
+++ os/os.js
@@ -0,0 +1,26 @@
+var os = require('os')
+
+console.log(os.tmpdir());
+console.log(os.endianness());
+console.log(os.hostname());
+console.log(os.type());
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.release());
+console.log(os.uptime());
+console.log(os.loadavg());
+console.log(os.totalmem());
+console.log(os.freemem());
+// console.log(os.cpus());
+// console.log(os.networkInterfaces());
+// /var/folders/rw/g35crtbj2_z2qf1zg20m49w00000gn/T
+// LE
+// AbunuodeMacBook-Pro.local
+// Darwin
+// darwin
+// x64
+// 16.4.0
+// 33636
+// [ 4.27197265625, 2.62451171875, 1.96728515625 ]
+// 8589934592
+// 1229451264
