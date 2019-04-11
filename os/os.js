6d700a6ebe82c6740e27a621a4f1e6a39472ce25
Monologue <liangmingqing@modian.com>
2019-04-11 15:32:14 +0800

    u

--- os/os.js
+++ os/os.js
@@ -1,16 +1,20 @@
 var os = require('os')
 
-console.log(os.tmpdir());
-console.log(os.endianness());
-console.log(os.hostname());
-console.log(os.type());
-console.log(os.platform());
-console.log(os.arch());
-console.log(os.release());
-console.log(os.uptime());
-console.log(os.loadavg());
-console.log(os.totalmem());
-console.log(os.freemem());
+// console.log(os.cpus());
+// console.log(os.freemem());
+console.log(os.networkInterfaces().en0[0].address);//获取主机 ip
+// console.log(os.userInfo());
+// console.log(os.tmpdir());
+// console.log(os.endianness());
+// console.log(os.hostname());
+// console.log(os.type());
+// console.log(os.platform());
+// console.log(os.arch());
+// console.log(os.release());
+// console.log(os.uptime());
+// console.log(os.loadavg());
+// console.log(os.totalmem());
+// console.log(os.freemem());
 // console.log(os.cpus());
 // console.log(os.networkInterfaces());
 // /var/folders/rw/g35crtbj2_z2qf1zg20m49w00000gn/T
