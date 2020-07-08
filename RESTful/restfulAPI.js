d98c519dcedf3b2b1d44eb233b6cfbb0c33ac0ca
Abunuo <liangmingqing315@163.com>
2020-07-08 19:23:22 +0800

    express post

--- RESTful/restfulAPI.js
+++ RESTful/restfulAPI.js
@@ -4,6 +4,7 @@
 
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser');
 var fs = require('fs')
 var request = require('request')
 var user = {
@@ -14,14 +15,18 @@ var user = {
     'id': 4
   }
 }
+// 解析 post 方式 body 参数
+app.use(bodyParser.urlencoded({extended : true}));  // 处理 post 请求 application/x-www-form-urlencoded
+app.use(bodyParser.json()); // 处理 post 请求 application/json
 
 app.use('*', function(req,res,next) {
-    console.log(req.headers);
     next();
 })
 
 //获取用户列表
 app.get('/users', function(req, res) {
+    var query = req.query; // get 请求参数 （ search 参数）
+    console.log('query', query);
     fs.readFile(__dirname + '/user.json', 'utf-8', function(err, data) {
         // console.log(data);
         res.end(data)
@@ -29,6 +34,9 @@ app.get('/users', function(req, res) {
 })
 //添加用户信息
 app.post('/users', function(req, res) {
+  var body = req.body; // post 请求体
+  console.log('body', body);
+  console.log('body', body.name);
   fs.readFile(__dirname+'/user.json', 'utf8', function(err, data) {
     data = JSON.parse(data);
     data['user4'] = user['user4'];
@@ -41,6 +49,10 @@ app.post('/users', function(req, res) {
 
 //获取用户详细信息
 app.get('/user/:id', function(req, res) {
+  var query = req.query; // get 请求参数 （ search 参数）
+  console.log('query', query);
+  var params = req.params; // get 请求体  id
+  console.log('params', params);
   fs.readFile( __dirname + "/" + "user.json", 'utf8', function (err, data) {
        data = JSON.parse( data );
        var user = data["user" + req.params.id]
@@ -72,22 +84,26 @@ app.delete('/user/:id', function(req, res) {
    });
 })
 
-var server = app.listen({port:8081, host: '127.0.0.1'}, function() {
+var server = app.listen({port:8888, host: '127.0.0.1'}, function() {
   var host = server.address().address
   var port = server.address().port
 
   console.log('服务已启动，地址为：http://%s:%s', host, port);
 })
 
-var options = {
-  baseUrl: 'http://127.0.0.1:8081',
-  url: '/users',
-  method: 'get',
-  headers: {
-      'Referer': 'http://www.baidu.com'
-  }
-}
-request(options, function(err, res, body) {
-  if(err) return console.log(err);
-  console.log(body);
-})
+// var options = {
+//   baseUrl: 'http://127.0.0.1:8888',
+//   url: '/users',
+//   method: 'post',
+//   headers: {
+//       'Referer': 'http://www.baidu.com',
+//       'Content-type': 'application/json'
+//   },
+//   data: {
+//     user: 1231
+//   }
+// }
+// request(options, function(err, res, body) {
+//   if(err) return console.log(err);
+//   console.log(body);
+// })
