bb66d0caf269f3729a8d744528fe1dc2b07b4fe2
liangmingqing <liangmingqing@im30.com>
2020-11-23 14:52:54 +0800

    完善图片爬虫,去除小文件

--- imgfile/request.js
+++ imgfile/request.js
@@ -4,8 +4,9 @@
  */
 
 const fs = require('fs');
+const path = require('path');
 const request = require('request');
-const index = 0;
+const BasePath = './imgfile/images';
 
 const defaultParams = {
     url: '',
@@ -14,7 +15,7 @@ const defaultParams = {
 }
 
 function getImages(index) {
-  let url = `http://wallpaper.apc.360.cn/index.php?c=WallPaper&start=${index}&count=1&from=360chrome&a=getAppsByCategory&cid=6`
+  let url = `http://wallpaper.apc.360.cn/index.php?c=WallPaper&start=${index}&count=1&from=360chrome&a=getAppsByCategory&cid=36`
   let params = Object.assign({}, defaultParams, {url})
   request(params, function(err, httpRes, body) {
     const resData = body.data;
@@ -22,12 +23,34 @@ function getImages(index) {
       let url = item.url.replace("__85", "__100"),
         name = item.utag ? item.utag : new Date().getTime();
       let readStream = request(url),
-        writeStream = fs.createWriteStream(`./imgfile/girl/${name}.jpg`);
+        writeStream = fs.createWriteStream(path.join(BasePath, `${name}.jpg`));
       await readStream.pipe(writeStream);
-      if(index < 400) {
+      if(index < 200) {
         getImages(++index)
       } else {
         console.log('下载完成');
+        filterImages();
+      }
+    })
+  })
+}
+function filterImages() {
+  let delFiles = [];
+  fs.readdir('./imgfile/images', (err,files) => {
+    files.forEach((fileName, index) => {
+      let filePath = path.join(BasePath, fileName);
+      fs.stat(filePath, (err, file) => {
+        if(file.size < 1024 * 1024 * 1.8) {
+          fs.unlink(filePath, function(err) {
+            if(err) {
+              return console.error(err)
+            }
+            delFiles.push(fileName);
+          })
+        }
+      })
+      if(index == files.length - 1) {
+        console.log('小文件处理完成，处理文件有：',delFiles);
       }
     })
   })
